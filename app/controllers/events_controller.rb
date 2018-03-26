c5ccf999309eafa7636c358a66fc3016a85dc951
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-03-27 07:57:37 +0900

    Def events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,24 @@
 class EventsController < ApplicationController
   before_action :authenticate
 
-  #TODO : 187ページから
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if current_user.save
+      redirect_to @event, notice: '作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+        :name, :place, :content, :start_time, :end_time
+      )
+  end
 end
